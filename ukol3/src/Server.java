8c52b0a54f4c3dd543517a37e74ca98b4762cd4f
Matyáš Balcar <matyasbalcar0@gmail.com>
2025-03-04 12:15:10 +0100

    optimzied imports

--- ukol3/src/Server.java
+++ ukol3/src/Server.java
@@ -1,8 +1,12 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 
 public class Server {
